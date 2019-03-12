3755905d74c96d9060a6611f0888701b7e1e7cc6
Yuhei Abe <yuhei@Yuhei-no-MacBook-Air.local>
2019-03-12 17:00:10 +0800

    Add Access Restriction

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/users/index', to: 'users#index'
   post '/signup', to: 'users#create'
   get '/dashboard', to: 'sessions#show'
-  get 'login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users # ユーザー登録失敗時にURLが/userとなってトップページに戻る
