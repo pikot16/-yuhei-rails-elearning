c653c4031878efe02ec5403773eb2bd9fafd0ee8
Yuhei Abe <yuhei@Yuhei-no-MacBook-Air.local>
2019-03-15 14:30:36 +0800

    Admin_User_Function

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,41 @@
+class Admin::UsersController < AdminController
+  before_action :logged_in_user, only: [:index, :update]
+  # before_action :correct_user, only: [:edit, :update] これ書いたままで Set As Adminをすると何も起きずにTopに戻ってしまう。
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.admin == true
+      @user.admin = false
+      @user.save # @user.update(admin = false)
+    else
+      @user.admin = true
+      @user.save # @user.update(admin = true)
+    end
+    redirect_to admin_users_path
+  end  
+  
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation, :agreement)
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please Log In"
+      redirect_to root_url
+    end
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:info] = "Please Log In"
+      redirect_to root_url
+    end
+  end  
+
+end
