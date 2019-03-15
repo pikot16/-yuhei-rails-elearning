85aae348cdbcb89a0e0ea594607c188a0bd8299a
Yuhei Abe <44052206+pikot16@users.noreply.github.com>
2019-03-15 14:53:47 +0800

    Admin_User_Function (#7)
    * Admin_User_Function
    
    * Refactor Update Action

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users # ユーザー登録失敗時にURLが/userとなってトップページに戻る
+  # resources :users # ユーザー登録失敗時にURLが/userとなってトップページに戻る
   resources :relationships, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users do
     member do
       get :following, :followers
     end
+  end
+
+  namespace :admin do
+    resources :users, only: [:index, :update]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_093156) do
+ActiveRecord::Schema.define(version: 2019_03_14_070123) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "follower_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_093156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
