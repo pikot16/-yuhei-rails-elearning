a77777ef5c75eeed66c16605195adeecc96a4c44
Yuhei Abe <yuhei@Yuhei-no-MacBook-Air.local>
2019-03-25 17:49:08 +0800

    Result Page

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,4 +1,6 @@
 class Choice < ApplicationRecord
+  has_many :answers, dependent: :destroy
+  has_many :lessons, through: :answers
   belongs_to :word
   validates :content, presence: true, length: { maximum:50, minimum:1 } # 必要かどうか
 end
